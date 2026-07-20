ddb6ac55ef504b7e45f08380164499497b18b8db
alane61 <alan@why-marketing.com>
2026-07-20 18:01:57 +0100

    Refactor Firebase setup for database integration
    Updated Firebase imports and configuration for database usage.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTfNOCNXmgk2iFZM16wFnBfujE8-_t1Ew",
-  authDomain: "project-toadstool.firebaseapp.com",
-  databaseURL: "https://project-toadstool-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "project-toadstool",
-  storageBucket: "project-toadstool.firebasestorage.app",
-  messagingSenderId: "590292971138",
-  appId: "1:590292971138:web:08b04b3493b4fbbc6c4e9b",
-  measurementId: "G-JZSRFRTLNZ"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+console.log("Toadstool Terminal Initialised.");
